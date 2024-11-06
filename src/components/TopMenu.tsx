2b447e669d6d28eeb85e1f2e83b3115f2815eed0
Phumsirii <phumsirisumativit@gmail.com>
2024-11-06 18:17:29 +0700

    add:reservations button

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -33,6 +33,15 @@ export default async function TopMenu() {
           </div>
         </Link>
       </div>
+
+      <div className="h-full hover:bg-gray-700 rounded-lg">
+        <Link href="/reservations">
+          <div className="flex items-center h-full px-5 text-cyan-600 text-xl">
+            Reservations
+          </div>
+        </Link>
+      </div>
+
     </div>
   );
 }
