b0213ed979f94e6bca470d18b38191983f873fb1
Phumsirii <phumsirisumativit@gmail.com>
2024-11-06 20:51:45 +0700

    edit: picker display

--- src/components/BookingDateAndTime.tsx
+++ src/components/BookingDateAndTime.tsx
@@ -12,17 +12,23 @@ export default function BookingDateAndTime({onDateChange, onTimeChange}:{onDateC
     const [bookingTime, setBookingTime] = useState<Dayjs | null>(null);
 
     return(
-        <div className="flex flex-col w-full my-6">
+        <div className="flex flex-col w-full my-6 mx-5 text-xl font-sans font-bold">
+            Pick Your Booking Date and Time
             <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DatePicker className='bg-white' value={bookingDate}
-                    onChange={(value) => { 
-                    setBookingDate(value);
-                    onDateChange(value) 
-                }} />
-                <TimePicker className='bg-white' value={bookingTime} onChange={(value) => { 
-                    setBookingTime(value);
-                    onTimeChange(value) 
-                }} />
+                <div className="my-3">
+                    <DatePicker className='bg-white' value={bookingDate}
+                        onChange={(value) => { 
+                        setBookingDate(value);
+                        onDateChange(value) 
+                    }} />
+                </div>
+                <div className="my-3">
+                    <TimePicker className='bg-white' value={bookingTime} ampm={false}
+                        onChange={(value) => { 
+                        setBookingTime(value);
+                        onTimeChange(value) 
+                    }} />
+                </div>
             </LocalizationProvider>
         </div>
     )
