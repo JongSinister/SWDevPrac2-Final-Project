304e00fc19a1cc7742245b1fac1a3eff2711fe2d
Phumsirii <phumsirisumativit@gmail.com>
2024-11-06 14:53:30 +0700

    add /restaurant page

--- src/app/(restaurantinfo)/restaurant/[rid]/page.tsx
+++ src/app/(restaurantinfo)/restaurant/[rid]/page.tsx
@@ -1,1 +1,18 @@
 import getRestaurant from "@/libs/getRestaurant";
+
+export default async function restaurantDetailPage({params}:{params:{rid:string}}){
+
+    const restaurant = await getRestaurant(params.rid);
+
+    return(
+        <div>
+            <h1>{restaurant.name}</h1>
+            <p>{restaurant.address}</p>
+            <p>{restaurant.foodtype}</p>
+            <p>{restaurant.province}</p>
+            <p>{restaurant.postalcode}</p>
+            <p>{restaurant.tel}</p>
+            <img src={restaurant.picture} alt={restaurant.name}/>
+        </div>
+    )
+}
