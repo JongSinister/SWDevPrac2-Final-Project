210edefa1dd5a7e9fef7479377d5cc4e8f1a39f9
Phumsirii <phumsirisumativit@gmail.com>
2024-11-06 15:03:25 +0700

    add: phone icon

--- src/app/(restaurantinfo)/restaurant/[rid]/page.tsx
+++ src/app/(restaurantinfo)/restaurant/[rid]/page.tsx
@@ -1,18 +1,16 @@
 import getRestaurant from "@/libs/getRestaurant";
+import Image from "next/image";
 
 export default async function restaurantDetailPage({params}:{params:{rid:string}}){
 
     const restaurant = await getRestaurant(params.rid);
 
     return(
-        <div>
-            <h1>{restaurant.name}</h1>
-            <p>{restaurant.address}</p>
-            <p>{restaurant.foodtype}</p>
-            <p>{restaurant.province}</p>
-            <p>{restaurant.postalcode}</p>
-            <p>{restaurant.tel}</p>
-            <img src={restaurant.picture} alt={restaurant.name}/>
+        <div className="mx-2 text-center font-sans p-5">
+            <h1 className="text-5xl font-bold">{restaurant.data.name}</h1>
+            <div className="flex flex-row my-10">
+                <Image src={restaurant.data.picture} alt={restaurant.data.name} width={1000} height={300} />
+            </div>
         </div>
     )
 }
