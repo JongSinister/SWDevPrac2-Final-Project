f7ced234364aee10cd7e8aec289bd83b811e367d
Phumsirii <phumsirisumativit@gmail.com>
2024-11-06 17:53:42 +0700

    add: login logout button

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -1,17 +1,27 @@
 import Link from "next/link";
 import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
-export default function TopMenu() {
+export default async function TopMenu() {
 
-  const session = getServerSession();
+  const session = await getServerSession(authOptions);
 
   return (
     <div className="h-[50px] bg-gray-900 fixed top-[0px] inset-x-[0px] z-30 flex flex-row items-center">
-      <Link href="/booking">
-        <div className="flex items-center h-full px-5 text-cyan-600 text-sm hover:underline">
-          Login
-        </div>
-      </Link>
+      {
+        session?
+        <Link href="/booking">
+          <div className="flex items-center h-full px-5 text-cyan-600 text-sm hover:underline">
+            Log Out
+          </div>
+        </Link>
+        :
+        <Link href="/booking">
+          <div className="flex items-center h-full px-5 text-cyan-600 text-sm hover:underline">
+            Log In
+          </div>
+        </Link>
+      }
       <Link href="/booking">
         <div className="flex items-center h-full px-5 text-cyan-600 text-sm hover:underline">
           Booking
