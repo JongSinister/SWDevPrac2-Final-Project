233bb9548855aa7607d8dd3d714441c3dac82279
Phumsirii <phumsirisumativit@gmail.com>
2024-11-06 14:13:58 +0700

    add: Restaurant Interface

---
+++ interface.ts
@@ -0,0 +1,12 @@
+interface RestaurantItem{
+    _id: string,
+    name: string,
+    address: string,
+    foodtype: string,
+    province: string,
+    postalcode: string,
+    tel: string,
+    picture: string,
+    __v: number,
+    id: string
+}
