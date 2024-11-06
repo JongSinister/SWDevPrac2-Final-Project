c881e42ca9f1eb0ce23662ff82cd2fad910b5b62
Phumsirii <phumsirisumativit@gmail.com>
2024-11-06 21:14:56 +0700

    add: name fill

--- src/app/booking/page.tsx
+++ src/app/booking/page.tsx
@@ -4,11 +4,13 @@ import { useState } from "react";
 import { Dayjs } from "dayjs";
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import BookingDateAndTime from "@/components/BookingDateAndTime";
+import { TextField } from "@mui/material";
 
 export default function bookings() {
 
   const [bookingDate, setBookingDate] = useState<Dayjs | null>(null);
   const [bookingTime, setBookingTime] = useState<Dayjs | null>(null);
+  const [name, setName] = useState<string>('');
 
   return (
     <div className="m-5">
@@ -16,6 +18,8 @@ export default function bookings() {
         Book Your Best Restaurant
       </div>
       <div className="mx-20 flex flex-col font-sans font-bold text-xl w-full">
+        <TextField name="Name" label="Name" fullWidth className="my-5 min-w-64 max-w-xl" variant="standard"
+                            value={name} onChange={(e)=>{setName(e.target.value)}}/>
         <BookingDateAndTime onDateChange={(value:Dayjs) => { setBookingDate(value) }} 
                           onTimeChange={(value:Dayjs) => { setBookingTime(value) }} />
       </div>
