5b72f7fbe86f2045181050e7771d6c1a875e144f
Phumsirii <phumsirisumativit@gmail.com>
2024-11-06 18:07:18 +0700

    edit: round top menu item

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -9,7 +9,7 @@ export default async function TopMenu() {
   return (
     <div className="h-[50px] bg-gray-900 fixed top-[0px] inset-x-[0px] z-30 flex flex-row items-center font-sans font-bold">
 
-      <div className="h-full hover:bg-gray-700">
+      <div className="h-full hover:bg-gray-700 rounded-lg">
       {
         session?
         <Link href="/api/auth/signin" className="h-full">
@@ -26,7 +26,7 @@ export default async function TopMenu() {
       }
       </div>
 
-      <div className="h-full hover:bg-gray-700">
+      <div className="h-full hover:bg-gray-700 rounded-lg">
         <Link href="/booking">
           <div className="flex items-center h-full px-5 text-cyan-600 text-xl">
             Booking
