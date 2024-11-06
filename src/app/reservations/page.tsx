4e1cf78ffd2a3abe97561a97214e48deaaff8ffe
Phumsirii <phumsirisumativit@gmail.com>
2024-11-06 18:23:10 +0700

    install date picker

---
+++ src/app/reservations/page.tsx
@@ -0,0 +1,11 @@
+import LocationDate
+
+export default function reservations() {
+  return (
+    <div className="m-5">
+      <div className="text-5xl font-sans font-bold">
+        Reservation
+      </div>
+    </div>
+  );
+}
