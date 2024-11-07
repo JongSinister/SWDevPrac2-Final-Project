3488a55500a76d317216aa28e8af0d91ae4e611d
Phumsirii <phumsirisumativit@gmail.com>
2024-11-07 19:33:46 +0700

    import elements in get restaurants page

--- src/app/(restaurantinfo)/restaurant/page.tsx
+++ src/app/(restaurantinfo)/restaurant/page.tsx
@@ -1,5 +1,7 @@
 import RestaurantCard from "@/components/RestaurantCard";
 import getRestaurants from "@/libs/getRestaurants";
+import { Suspense } from "react";
+import { LinearProgress } from "@mui/material";
 
 export default function restaurants(){
     
@@ -8,6 +10,9 @@ export default function restaurants(){
     return(
         <div className="font-sans m-6 text-3xl font-bold">
             Restaurants
+            {/* <Suspense fallback={<p>Loading ... <LinearProgress/> </p>}>
+            {hospitals && <HospitalCatalog hospitalsJson={hospitals}/>}
+            </Suspense> */}
             <RestaurantCard imgSrc="https://drive.google.com/uc?id=1gx8WSQ3kByzVs8eQs3o5DPqsWTSrzvLE" 
                             restaurantName="Mama Paradox" foodtype="Japanese"/>          
         </div>
