0d747148e47328ce98b861c1a786c4ff6aaaa9ae
Jeerasak Kajonrattanavanit <mazuroze@gmail.com>
2024-11-07 22:28:41 +0700

    create some input

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,13 @@
+import { TextField } from "@mui/material";
+
 export default function adminPage() {
-    return (
-        <div>
-            <h1>Admin Page</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Create Restaurant</h1>
+      <div>
+        <TextField id="name" label="Name" variant="outlined" size="small" />
+        <TextField id="foodtype" label="Food Type" variant="outlined" size="small" />
+      </div>
+    </div>
+  );
 }
