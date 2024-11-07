4d7d4bb9dd4859657c5634c411152c8b64bca6a8
Phumsirii <phumsirisumativit@gmail.com>
2024-11-07 20:35:27 +0700

    check server session

--- src/components/RestaurantSelector.tsx
+++ src/components/RestaurantSelector.tsx
@@ -1,11 +1,17 @@
 import Link from "next/link";
 import RestaurantCard from "./RestaurantCard";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
 
 export default async function RestaurantSelector({restaurants}:{restaurants:Promise<RestaurantJSON>}) {
 
     const restaurantList = await restaurants;
+    const session = await getServerSession(authOptions);
+    console.log(session);
+    let isAdmin = false;
+    // if(session){
+    //     isAdmin = session.user?.role==="admin";
+    // }
     
     return (
         <div className="flex flex-col m-5 w-full justify-center">
@@ -15,6 +21,7 @@ export default async function RestaurantSelector({restaurants}:{restaurants:Prom
                 imgSrc={restaurant.picture}
                 restaurantName={restaurant.name}
                 foodtype={restaurant.foodtype}
+                isAdmin={isAdmin}
                 />
             </Link>
         ))}
