dc851518396c37656d680909a01aa5c244bea05a
Phumsirii <phumsirisumativit@gmail.com>
2024-11-07 19:10:16 +0700

    edit: restaurant image style

--- src/components/RestaurantCard.tsx
+++ src/components/RestaurantCard.tsx
@@ -4,8 +4,8 @@ import Image from "next/image"
 export default function RestaurantCard({imgSrc,restaurantName,foodtype}:{imgSrc:string,restaurantName:string,foodtype:string}) {
     return(
         <div className="font-sans m-3 bg-gray-400 text-xl max-w-3xl h-40">
-            <div className="flex flex-row">
-                <Image src={imgSrc} alt={restaurantName} className="w-1/3"/>
+            <div className="flex flex-row h-full">
+                <Image src={imgSrc} alt={restaurantName} className="w-1/3 h-full object-cover object-center"/>
             </div>
         </div>
     )
