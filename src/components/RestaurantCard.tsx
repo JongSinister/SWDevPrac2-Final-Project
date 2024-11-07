15e786d0f869a34c37e5bac1cc6c0c15da1718d4
Phumsirii <phumsirisumativit@gmail.com>
2024-11-07 18:12:24 +0700

    edit: restaurant card

--- src/components/RestaurantCard.tsx
+++ src/components/RestaurantCard.tsx
@@ -3,9 +3,10 @@ import Image from "next/image"
 
 export default function RestaurantCard({imgSrc,restaurantName,foodtype}:{imgSrc:string,restaurantName:string,foodtype:string}) {
     return(
-        <div className="font-sans m-3 bg-gray-400 text-xl max-w-3xl miin-w-full h-40">
-            RestaurantCard
-            <Image src="/img/restaurant.jpg" alt="restaurant" width={1000} height={300}/>
+        <div className="font-sans m-3 bg-gray-400 text-xl max-w-3xl h-40">
+            <div className="flex flex-row">
+                <Image src={imgSrc} alt={restaurantName} className="w-1/3"/>
+            </div>
         </div>
     )
 }
