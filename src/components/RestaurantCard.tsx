b1db318f06a8660b8ff1d452c5abfba2e73c7ba5
Phumsirii <phumsirisumativit@gmail.com>
2024-11-07 19:59:42 +0700

    add: card hover

--- src/components/RestaurantCard.tsx
+++ src/components/RestaurantCard.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 
 export default function RestaurantCard({imgSrc,restaurantName,foodtype}:{imgSrc:string,restaurantName:string,foodtype:string}) {
     return(
-        <div className="font-sans mx-3 bg-gray-400 text-xl max-w-3xl h-40 flex flex-row rounded-xl w-full">
+        <div className="font-sans mx-3 bg-gray-400 text-xl max-w-3xl h-40 flex flex-row rounded-xl w-full hover:bg-gray-300">
             <div className="relative h-full w-1/3">
                 <Image src={imgSrc} alt={restaurantName} fill={true} className='object-cover rounded-xl w-1/3'/>
             </div>
