bb5d261bf3283e46d5adaac72f5dc788e16ac650
Phumsirii <phumsirisumativit@gmail.com>
2024-11-07 19:43:19 +0700

    feat: display all restaurants card

--- src/app/(restaurantinfo)/restaurant/page.tsx
+++ src/app/(restaurantinfo)/restaurant/page.tsx
@@ -2,6 +2,7 @@ import RestaurantCard from "@/components/RestaurantCard";
 import getRestaurants from "@/libs/getRestaurants";
 import { Suspense } from "react";
 import { LinearProgress } from "@mui/material";
+import RestaurantSelector from "@/components/RestaurantSelector";
 
 export default function restaurants(){
     
@@ -10,11 +11,9 @@ export default function restaurants(){
     return(
         <div className="font-sans m-6 text-3xl font-bold">
             Restaurants
-            {/* <Suspense fallback={<p>Loading ... <LinearProgress/> </p>}>
-            {hospitals && <HospitalCatalog hospitalsJson={hospitals}/>}
-            </Suspense> */}
-            <RestaurantCard imgSrc="https://drive.google.com/uc?id=1gx8WSQ3kByzVs8eQs3o5DPqsWTSrzvLE" 
-                            restaurantName="Mama Paradox" foodtype="Japanese"/>          
+            <Suspense fallback={<p>Loading ... <LinearProgress/> </p>}>
+            {restaurants && <RestaurantSelector restaurants={restaurants}/>}
+            </Suspense>
         </div>
     )
 
