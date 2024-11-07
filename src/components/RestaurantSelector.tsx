ef5b9d205b4fa8c0bf573fefd64dd5a611245c81
Phumsirii <phumsirisumativit@gmail.com>
2024-11-07 20:40:10 +0700

    edit: userLogin

--- src/components/RestaurantSelector.tsx
+++ src/components/RestaurantSelector.tsx
@@ -7,7 +7,7 @@ export default async function RestaurantSelector({restaurants}:{restaurants:Prom
 
     const restaurantList = await restaurants;
     const session = await getServerSession(authOptions);
-    console.log(session);
+    console.log("session : "+ session);
     let isAdmin = false;
     // if(session){
     //     isAdmin = session.user?.role==="admin";
--- src/libs/userLogIn.tsx
+++ src/libs/userLogIn.tsx
@@ -13,6 +13,5 @@ export default async function userLogin(userEmail: string, userPassword: string)
     if(!response.ok){
         throw new Error("Failed to login")
     }
-    console.log(response)
     return await response.json()
 }
