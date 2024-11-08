508a9b391b3589cd1d2305abc7925984f667abd1
Phumsirii <phumsirisumativit@gmail.com>
2024-11-08 20:30:13 +0700

    feat: BookingCard using mock data

--- src/app/view-booking/page.tsx
+++ src/app/view-booking/page.tsx
@@ -1,7 +1,19 @@
+import { Suspense } from "react"
+import { LinearProgress } from "@mui/material"
+import BookingCard from "@/components/BookingCard"
+
 export default function viewBookingPage(){
     return (
         <div className="font-sans m-6 text-3xl font-bold">
             View Bookings
+            <div className="flex justify-center">
+                {/* <Suspense fallback={<p>Loading ... <LinearProgress/> </p>}>
+                {restaurants && <RestaurantSelector restaurants={restaurants}/>}
+                </Suspense> */}
+                <BookingCard bookingDate={new Date()} restaurantName="" restaurantTel="" 
+                            restaurantID="672dfaa2d7821c2e36b8212e" address="" 
+                            numOfGuests={3}/>
+            </div>
         </div>
     )
 }
--- src/components/BookingCard.tsx
+++ src/components/BookingCard.tsx
@@ -1,11 +1,16 @@
 'use client'
 import Image from "next/image"
+import Link from "next/link"
 
-export default function BookingCard({bookingDate,restaurantName,address,restaurantTel,numOfGuests}:
-    {bookingDate:Date,restaurantName:string,address:string,restaurantTel:string,numOfGuests:Number}) {
+export default function BookingCard({bookingDate,restaurantName,address,restaurantTel,numOfGuests,restaurantID}:
+    {bookingDate:Date,restaurantName:string,address:string,restaurantTel:string,numOfGuests:Number,restaurantID:string}) {
     return(
         <div className="font-sans mx-3 bg-gray-200 text-xl max-w-3xl h-40 flex flex-row rounded-xl w-full hover:bg-gray-300 hover:shadow-xl">
-            
+            <Link href={`/restaurant/${restaurantID}`} className="w-full h-full">
+                <div>
+                    This is a Booking
+                </div>
+            </Link>
         </div>
     )
 }
