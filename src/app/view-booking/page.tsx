398515d4af754a6eff2420ecc94bec539eb57d37
Phumsirii <phumsirisumativit@gmail.com>
2024-11-08 21:39:30 +0700

    add:session checking in view-booking

--- src/app/view-booking/page.tsx
+++ src/app/view-booking/page.tsx
@@ -3,19 +3,34 @@ import { LinearProgress } from "@mui/material"
 import BookingCard from "@/components/BookingCard"
 import getBookings from "@/libs/getBookings"
 import BookingList from "@/components/BookingList"
+import { getServerSession } from "next-auth"
+import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions"
 
 export default async function viewBookingPage(){
 
     const bookings=await getBookings()
+    const session = await getServerSession(authOptions);
 
     return (
         <div className="font-sans m-6 text-3xl font-bold">
-            View Bookings
-            <div className="flex justify-center m-10">
-                <Suspense fallback={<p>Loading ... <LinearProgress/> </p>}>
-                {bookings && <BookingList bookings={bookings}/>}
-                </Suspense>
-            </div>
+            {
+                session ? (
+                    <div>
+                        <div className="font-sans m-6 text-3xl font-bold">
+                            View Bookings
+                        </div>
+                        <div className="flex justify-center m-10">
+                            <Suspense fallback={<p>Loading ... <LinearProgress/> </p>}>
+                            {bookings && <BookingList bookings={bookings}/>}
+                            </Suspense>
+                        </div>
+                    </div>
+                ):(
+                    <div className="font-sans m-6 text-3xl font-bold">
+                        Please login to view bookings.
+                    </div>
+                )
+            }
         </div>
     )
 }
