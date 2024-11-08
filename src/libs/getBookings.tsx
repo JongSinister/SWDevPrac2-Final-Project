e5613704259107e08e346af7f3f93d9beec2c263
Phumsirii <phumsirisumativit@gmail.com>
2024-11-08 21:50:59 +0700

    edit getBookings

--- src/libs/getBookings.tsx
+++ src/libs/getBookings.tsx
@@ -1,6 +1,18 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
+
 export default async function getBookings() {
+    
+    const session = await getServerSession(authOptions);
+
     const response = await fetch(
-      "https://restaurant-booking-project-backend.vercel.app/api/v1/bookings"
+      "https://restaurant-booking-project-backend.vercel.app/api/v1/bookings",{
+        method:"GET",
+        headers:{
+            "Content-Type":"application/json",
+            "Authorization": `Bearer ${session?.user.token}`,
+        },
+      }
     );
     if (!response.ok) {
       throw new Error("Failed to fetch restaurants");
