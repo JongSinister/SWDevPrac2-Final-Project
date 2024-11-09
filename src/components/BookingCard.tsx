77fc6a5840820325883b6c5950b33cc161271239
Phumsirii <phumsirisumativit@gmail.com>
2024-11-09 21:27:46 +0700

    edit: booking card appearance

--- src/components/BookingCard.tsx
+++ src/components/BookingCard.tsx
@@ -31,7 +31,7 @@ export default function ReservationCard({bookingDate,restaurantName,address,rest
                         </div>
                         <div className="flex flex-row">
                             <Image src="/img/guests.png" alt="" width={30} height={30} className="mr-6" />
-                            {numOfGuests.toString()} Guests
+                            {numOfGuests.toString()} Guest{numOfGuests>1 ? "s":""}
                         </div>
                     </div>
                 </div>
