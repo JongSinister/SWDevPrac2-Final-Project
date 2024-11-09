b44759f6b2b96652f3e72761f4375f7facae0399
Jeerasak Kajonrattanavanit <mazuroze@gmail.com>
2024-11-09 23:16:35 +0700

    add createRestaurant system ro CreateRestaurantBoard

--- src/components/CreateRestaurantBoard.tsx
+++ src/components/CreateRestaurantBoard.tsx
@@ -1,14 +1,29 @@
 "use client";
 import { TextField } from "@mui/material";
 import { useState } from "react";
+import createRestaurant from "@/libs/createRestaurant";
 
 export default function CreateRestaurantBoard() {
   const [name, setName] = useState("");
   const [address, setAddress] = useState("");
   const [foodType, setFoodType] = useState("");
   const [province, setProvince] = useState("");
+  const [postalcode, setPostalcode] = useState("");
   const [tel, setTel] = useState("");
   const [picture, setPicture] = useState("");
+
+  const handleCreateRestaurant = async () => {
+    const restaurant = {
+      name,
+      address,
+      foodtype: foodType,
+      province,
+      postalcode,
+      tel,
+      picture,
+    } as RestaurantItem;
+    await createRestaurant(restaurant);
+  };
 
   return (
     <div className="flex flex-col w-64 space-y-2 ml-5 mt-2 bg-slate-100 p-2 rounded-md">
@@ -47,6 +62,14 @@ export default function CreateRestaurantBoard() {
       />
       <TextField
         className="bg-white rounded-md"
+        id="postalcode"
+        label="Postal Code"
+        variant="outlined"
+        size="small"
+        onChange={(e) => setPostalcode(e.target.value)}
+      />
+      <TextField
+        className="bg-white rounded-md"
         id="tel"
         label="Telephone Number"
         variant="outlined"
@@ -61,7 +84,7 @@ export default function CreateRestaurantBoard() {
         size="small"
         onChange={(e) => setPicture(e.target.value)}
       />
-      <button className="bg-green-400 hover:bg-green-600 rounded-lg w-2/3 h-8">
+      <button className="bg-green-400 hover:bg-green-600 rounded-lg w-2/3 h-8" onClick={handleCreateRestaurant}>
         Create Restaurant
       </button>
     </div>
