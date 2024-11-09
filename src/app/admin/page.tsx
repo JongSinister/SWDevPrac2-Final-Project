57e6995934182b7c6e9001f8c9a106c693989955
Jeerasak Kajonrattanavanit <mazuroze@gmail.com>
2024-11-09 23:28:45 +0700

    add admin page navigation

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,10 +1,26 @@
 import CreateRestaurantBoard from "@/components/CreateRestaurantBoard";
+import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
+import { getServerSession } from "next-auth";
+import getMe from "@/libs/getMe";
 
-export default function adminPage() {
+export default async function adminPage() {
+  const session = await getServerSession(authOptions);
+  console.log("session : " + session);
+  let isAdmin = false;
+  if (session) {
+    isAdmin = (await getMe()).data.role === "admin";
+  }
+
   return (
     <div className="ml-2">
-      <h1 className="font-medium text-lg ml-3">Create Restaurant</h1>
-      <CreateRestaurantBoard />
+      {isAdmin ? (
+        <>
+          <h1 className="font-medium text-lg ml-3">Create Restaurant</h1>
+          <CreateRestaurantBoard />
+        </>
+      ) : (
+        <div>You don't have permission to enter This Route</div>
+      )}
     </div>
   );
 }
