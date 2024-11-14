0b84cd9bd70242a7890012e6bab0fe492a4af908
Phumsirii <phumsirisumativit@gmail.com>
2024-11-14 22:42:20 +0700

    add: test admin restaurant card

--- __tests__/test1.test.tsx
+++ __tests__/test1.test.tsx
@@ -3,6 +3,8 @@ import {useSession} from "next-auth/react";
 import {userEvent} from '@testing-library/user-event'
 import '@testing-library/jest-dom';
 import Banner from "@/components/Banner";
+import { get } from "http";
+import RestaurantCard from "@/components/RestaurantCard";
 
 jest.mock('next/navigation', () => ({
   useRouter: () => ({
@@ -12,11 +14,22 @@ jest.mock('next/navigation', () => ({
   }),
 }));
 
-
 describe("Admin views",()=>{
+
     it("Should exists an admin restaurant console",async()=>{
       render(<Banner isAdmin={true} />);
       const adminConsoleText = screen.getByText("Admin Restaurant Console");
       expect(adminConsoleText).toBeInTheDocument();
+    })
+
+    it("Should exists an admin restaurant console",async()=>{
+      const mockOnDelete = jest.fn();
+      render(<RestaurantCard id={"672fc12b6291757b2b4ccee0"} imgSrc={"/img/date.png"} 
+        restaurantName={"MK"} foodtype={"general"} 
+        isAdmin={true} onDelete={mockOnDelete} />);
+      expect(screen.getByText("MK")).toBeInTheDocument();
+      expect(screen.getByText("general Food")).toBeInTheDocument();
+      const deleteButton = screen.getByText("Delete");
+      expect(deleteButton).toBeInTheDocument();
     })
 })
